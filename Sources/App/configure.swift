86e3697c10e7c0e2de13cf82265333f82db25661
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2020-01-01 14:23:19 -0500

    run vapor with postgresql

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,5 @@
 import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 import AppModels
 
@@ -8,6 +9,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     // Register providers first
     try services.register(FluentSQLiteProvider())
+    try services.register(FluentPostgreSQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -21,25 +23,32 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    var sqlite: SQLiteDatabase
+    var postgres: PostgreSQLDatabase
        
-       if env.isRelease {
+//       if env.isRelease {
            /// Create file-based SQLite db using $SQLITE_PATH from process env
            /// Use the static method Environment.get(_:) to fetch string values from the process environment.
-           sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!)) //db.sqlite url??
-       } else {
-           /// Create an in-memory SQLite database
-           sqlite = try SQLiteDatabase(storage: .memory)
-       }
-       services.register(sqlite)
+//           sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!)) //db.sqlite url??
+        let database = Environment.get("POSTGRES_DB")
+        let user = Environment.get("POSTGRES_USER")
+        let password = Environment.get("POSTGRES_PASSWORD")
+        let config = PostgreSQLDatabaseConfig(hostname: "localhost", username: "vapor", database: "vapor", password: "password")
+        postgres = PostgreSQLDatabase(config: config)
+//       } else {
+//           /// Create an in-memory SQLite database
+//        postgres = try PostgreSQLDatabase(config: .default())
+//       }
+    
+       services.register(postgres)
     
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: postgres, as: .psql)
     services.register(databases)
 
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Todo.self, database: .psql)
+    migrations.add(model: User.self, database: .psql)
     services.register(migrations)
 }
