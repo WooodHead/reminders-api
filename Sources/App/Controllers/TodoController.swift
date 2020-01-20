d583bf1a58949201c90cdf429157b4d923381316
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2020-01-19 18:31:12 -0600

    feature: feth the sibilings relationships

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -6,23 +6,41 @@ import Fluent
 final class TodoController: RouteCollection {
     // thread-safe architecture
     private let todoRespositroy: TodoRespositroy
+    
+    private let CategoriasKey = "categorias"
+    
     init(todoRespositroy: TodoRespositroy) {
         self.todoRespositroy = todoRespositroy
     }
     
     func boot(router: Router) throws {
         let todoRoutes = router.grouped("api", "todo")
+        todoRoutes.get("all", use: indexHandler)
         todoRoutes.get(use: todoByTitleOrIDHandler)
         todoRoutes.get(use: todoByIdQueryParamsHandler) //  1. URLQueryParams ≈ req.query[Type.self, at: :key]
         todoRoutes.post(Todo.self, use: createHandler) //3. decodes the Todo object for the handler
         todoRoutes.put(Todo.parameter, use: updateTodoHandler)
         todoRoutes.delete(Todo.parameter, use: deleteHandler)
+        
+        // User association
         todoRoutes.get(Todo.parameter, "user", use: getUserHandler) // v1/api/todo/type_id/user | type_id ≈ Todo.parametes
-        todoRoutes.post(Todo.parameter, "categorias", Categoria.parameter, use: addCategoriasHandler)
+        
+        // Categories association
+        todoRoutes.post(Todo.parameter, CategoriasKey, Categoria.parameter, use: addCategoriasHandler)
+        todoRoutes.get(Todo.parameter, CategoriasKey, use: getCategoriasHandler)
+        
         // you dont need to specifiy the parameters type in the PathComponents for the func parameter
     }
     
     // MARK: -
+    ///
+    /// - Parameter req: Request
+    func todoByTitleOrIDHandler(_ req: Request) throws -> Future<[Todo]> {
+        return try todoRespositroy.findByParams(query: req.query)
+    }
+    
+    ///
+    /// - Parameter req:
     func todoByIdQueryParamsHandler(_ req: Request) throws -> Future<Todo> {
         guard let searchById = req.query[Int.self, at: "id"] else {
             throw Abort(.badRequest)
@@ -38,11 +56,8 @@ final class TodoController: RouteCollection {
         return todoRespositroy.find(id: id)
     }
     
-    func todoByTitleOrIDHandler(_ req: Request) throws -> Future<[Todo]> {
-        return try todoRespositroy.findByParams(query: req.query)
-    }
-    
     /// Updates the decoded Todo in the database
+    /// - Parameter req: Request
     func updateTodoHandler(_ req: Request) throws -> Future<Todo> {
         return try flatMap(to: Todo.self,
                            req.parameters.next(Todo.self), //find the Todo in the DB??
@@ -51,6 +66,8 @@ final class TodoController: RouteCollection {
         }
     }
     
+    ///
+    /// - Parameter req:
     func getUserHandler(_ req: Request) throws -> Future<User> {
         return try req.parameters.next(Todo.self).flatMap(to: User.self, { (todo) in
             return self.todoRespositroy.user(forTodo: todo)
@@ -73,11 +90,18 @@ final class TodoController: RouteCollection {
         return todoRespositroy.delete(id)
     }
     
+    /// Set a reminder's category
     func addCategoriasHandler(_ req: Request) throws -> Future<HTTPStatus> {
         return try flatMap(to: HTTPStatus.self,
                            req.parameters.next(Todo.self),
                            req.parameters.next(Categoria.self), { (todo, categoria) in
-                            return todo.categorias.attach(categoria, on: req).transform(to: .ok)
+                            return todo.categorias.attach(categoria, on: req).transform(to: .created)
+        })
+    }
+    
+    func getCategoriasHandler(_ req: Request) throws -> Future<[Categoria]> {
+        return try req.parameters.next(Todo.self).flatMap(to: [Categoria].self, { (todo) in
+            try todo.categorias.query(on: req).all()
         })
     }
 }
